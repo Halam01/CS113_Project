4d752df21327b1afdb72f7f4b54ac8f8dffbcf88
Hanna George Alam <hanna.alam@aol.com>
2016-11-02 22:32:30 -0700

    Commit 11/2/2016
    Added Scenes 3 and 4 (floors 2 and 3). Made it so "player" object in
    each scene has flashlight controls. Added GameControl object and script
    to allow for persistent data (ie. player progress, state variables,
    etc.)

---
+++ CS113_Project/Assets/Scripts/DoNotDestroy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoNotDestroy : MonoBehaviour {
+
+    public static DoNotDestroy player;
+
+    // Use this for initialization
+    void Awake()
+    {
+        if (player == null)
+        {
+            DontDestroyOnLoad(this);
+            player = this;
+        }
+        else if (player != this)
+        {
+            Destroy(this);
+        }
+
+    }
+}
---
+++ CS113_Project/Assets/Scripts/GameControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControl : MonoBehaviour {
+
+    public static GameControl control;
+
+    public float health;
+    public float experience;
+
+	// Use this for initialization
+	void Awake () {
+        if(control == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            control = this;
+        }
+        else if (control != this)
+        {
+            Destroy(gameObject);
+        }
+        
+	}
+	
+	void OnGUI()
+    {
+        //temp variables, just to see how saving data works between scenes, can access these variables via control.method/variable
+        GUI.Label(new Rect(10, 10, 100, 30), "Health: " + health);
+        GUI.Label(new Rect(10, 40, 100, 30), "Experience: " + experience);
+    }
+}
