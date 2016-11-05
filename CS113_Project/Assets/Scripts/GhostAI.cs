c3a505f94f377008efdfd70e9d9124a622970a3e
Xander Thompson <x.thompson3@gmail.com>
2016-11-05 16:36:23 -0700

    Light, Camera, Ghost
    Changed to dimmer level lighting, lowered level cameras to bring them
    closer to player, adjusted ghostAI script to find the actual range of
    the flashlight.

--- CS113_Project/Assets/Scripts/GhostAI.cs
+++ CS113_Project/Assets/Scripts/GhostAI.cs
@@ -5,18 +5,19 @@ using System;
 public class GhostAI : MonoBehaviour {
 
     bool on_hit = false;
-    double range = 10.0;
+    double range = 6;
     double angle = 15.0;
     float moveSpeed = 0.1f;
 
     // Update is called once per frame
     void Update() {
         GameObject player = GameObject.FindWithTag("Player");
-        GameObject light = GameObject.FindWithTag("Flashlight");
+        GameObject flashlight = GameObject.FindWithTag("Flashlight");
         Vector3 player_pos = player.transform.position;
+        range = (double)(flashlight.GetComponent<Light>().range)-2;
         Vector3 ghost_pos = transform.position;
  
-        if (Vector3.Distance(player_pos, ghost_pos) <= range && Vector3.Angle(light.transform.forward, ghost_pos - player_pos) < angle && on_hit == false)
+        if (Vector3.Distance(player_pos, ghost_pos) <= range && Vector3.Angle(flashlight.transform.forward, ghost_pos - player_pos) < angle && on_hit == false)
         {
             //transform.Rotate(0, Time.deltaTime * 30, 0, Space.Self);
             // transform.Translate(transform.forward * 100 * Time.deltaTime);
