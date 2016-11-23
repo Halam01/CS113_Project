1710bfea0f39cb8c02983e91fd13e65fac791484
Xander Thompson <x.thompson3@gmail.com>
2016-11-22 20:56:43 -0800

    Revert "Small adjustments"
    This reverts commit fe26b0f1e724154e647dd1d0352f2570f358d807.

--- CS113_Project/Assets/Scripts/CameraMovement.cs
+++ CS113_Project/Assets/Scripts/CameraMovement.cs
@@ -4,11 +4,11 @@ using System.Collections;
 public class CameraMovement : MonoBehaviour {
 
     public GameObject player;     
-    //private Vector3 offset;         
+    private Vector3 offset;         
     void Start()
     {
         //Calculate and store the offset value by getting the distance between the player's position and camera's position.
-        //offset = transform.position - player.transform.position;
+        offset = transform.position - player.transform.position;
         
     }
 
@@ -17,6 +17,7 @@ public class CameraMovement : MonoBehaviour {
     {
         // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
         //transform.position = player.transform.position + offset;
+        // Ideally, want to set the camera y position to never change. How?
         transform.position = new Vector3(player.transform.position.x, 10, player.transform.position.z);
     }
 }
